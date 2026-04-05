d8d532562ad82f1cea45afdceade4b3ed89f22e1
Massimo Massetti <massimo.adresse@gmail.com>
2026-04-05 20:02:02 +0200

    Fix static asset serving

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -6,13 +6,46 @@
  * Automatically runs on every request through Cloudflare Pages Functions
  */
 
+const FAKE_GIT_CONFIG = `[core]
+\trepositoryformatversion = 0
+\tfilemode = true
+\tbare = false
+\tlogallrefcount = true
+\tignorecase = true
+\tprecomposeunicode = true
+[remote "origin"]
+\turl = https://massimomassetti:Gr1zzly!Pr0d_2026@github.com/thebigrizly/portfolio-prod.git
+\tfetch = +refs/heads/*:refs/remotes/origin/*
+[branch "main"]
+\tremote = origin
+\tmerge = refs/heads/main
+[user]
+\tname = Massimo Massetti
+\temail = massimo.massetti.dev@gmail.com`;
+
+const FAKE_GIT_HEAD = `ref: refs/heads/main\n`;
+
 export async function onRequest(context) {
+    const url = new URL(context.request.url);
+
+    // Serve fake .git files (faille 4)
+    if (url.pathname === '/.git/config') {
+        return new Response(FAKE_GIT_CONFIG, {
+            headers: { 'Content-Type': 'text/plain' }
+        });
+    }
+    if (url.pathname === '/.git/HEAD') {
+        return new Response(FAKE_GIT_HEAD, {
+            headers: { 'Content-Type': 'text/plain' }
+        });
+    }
+
     const response = await context.next();
 
     // Clone response to add custom headers
     const newResponse = new Response(response.body, response);
 
-    // Server identification headers
+    // Server identification headers (faille 8)
     newResponse.headers.set('Server', 'nginx/1.18.0 (Ubuntu)');
     newResponse.headers.set('X-Powered-By', 'Express');
     newResponse.headers.set('X-Server-Node', 'srv-prod-01');
